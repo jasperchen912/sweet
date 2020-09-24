ff9bf541a47ecdc7bbac4ad559e8d9f8ad26052a
Jasper <jiajunch@cisco.com>
2020-09-24 10:45:07 +0800

    Init Project

---
+++ src/main/java/com/mumu/sweet/SweetApplication.java
@@ -0,0 +1,16 @@
+package com.mumu.sweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiajunch
+ */
+@SpringBootApplication
+public class SweetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweetApplication.class, args);
+    }
+
+}
